312048ab6104df6350233468c5e32179c68f6c97
Wesley <142677140+rattposion@users.noreply.github.com>
2025-07-01 13:22:43 -0300

    Delete src directory

--- src/utils/fbCapi.ts
+++
@@ -1,7 +0,0 @@
-export async function sendCapiEvent(event_name: string, user_data: any = {}, custom_data: any = {}) {
-  await fetch('http://localhost:3000/fb-capi', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ event_name, user_data, custom_data })
-  });
-} 
